78e4430df4767101272c8966b6f5daca41c1112a
2024310183 <2024310183@aluno.osorio.ifrs.edu.br>
2026-07-13 10:40:43 -0300

    mais bugs pra arrumar dps

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
 void main(String[] args) {
     new tela();
+    System.out.println("oi");
 }
--- src/tela.java
+++ src/tela.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class tela extends JFrame {
     public tela(){
         setTitle("Tela Inicial");
-        JOptionPane.showMessageDialog(this, "Resolvi Bug");
+        JOptionPane.showMessageDialog(this, "Resolvi Bugs");
         setVisible(true);
         setSize(200,200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
